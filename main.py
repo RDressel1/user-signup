d335dd98bc5ca11f99c78e369614a1aabc28a76a
Robert Dressel <robertdressel1@gmail.com>
2017-04-09 23:14:16 -0500

    ?

--- main.py
+++ main.py
@@ -42,84 +42,89 @@ page_footer = """
 </html>
 """
 
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+	return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+	return password and PASS_RE.match(password)
+
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+	return not email or EMAIL_RE.match(email)
+
 class Index(webapp2.RequestHandler):
-	def get(self):
-			forms = """
-			<form action="/verify" method="post">
-				<label>
-					Username:
-					<input type="text" name="username"/>
-				</label>
-				<br>
+    def get(self):
+        forms = """
+        <form method="post">
+        	<label>
+        		Username:
+        		<input type="text" name="username"/ value="{{username}}">
+        	</label>
+        	<br>
 
-				<label>
-					Password:
-					<input type="password" name="password"/>
-                    <span class = "error"></span>
-				</label>
-				<br>
+        	<label>
+        		Password:
+        		<input type="password" name="password" value=""/>
+                <span class = "error"></span>
+        	</label>
+        	<br>
 
-				<label>
-					Verify Password:
-					<input type="password" name="password_verify"/>
-				</label>
-				<br>
+        	<label>
+        		Verify Password:
+        		<input type="password" name="password_verify" value=""/>
+        	</label>
+        	<br>
 
-				<label>
-					Email (optional):
-					<input type="text" name="email_address"/>
-				</label>
-				<br>
+        	<label>
+        		Email (optional):
+        		<input type="text" name="email_address"/ value="">
+        	</label>
+        	<br>
 
-				<input type="submit" value="Submit"/>
-			</form>
-			"""
+        	<input type="submit" value="Submit"/>
+        </form>
+        """
+        content = page_header + forms + page_footer
+        self.response.write(content)
 
-			content = page_header + forms + page_footer
-			self.response.write(content)
-
-class Verification(webapp2.RequestHandler):
     def post(self):
     	username = self.request.get("username")
+        global_user = username
     	password = self.request.get("password")
     	ver_password = self.request.get("password_verify")
     	email = self.request.get("email_address")
+        error_ind = False
 
+        params = dict(username = username, email = email)
 
-        USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-    	def valid_username(username):
-    		return username and USER_RE.match(username)
-
-    	PASS_RE = re.compile(r"^.{3,20}$")
-    	def valid_password(password):
-    		return password and PASS_RE.match(password)
-
-    	EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
-    	def valid_email(email):
-    		return not email or EMAIL_RE.match(email)
-
-    	if not valid_username(username):
-    		error = "Invalid username!"
-    		self.redirect("/?error=" + error)
+        if not valid_username(username):
+            params['error_username'] = "Invalid username!"
+            error_ind = True
 
         if not valid_password(password):
-            error = "Invalid password!"
-            self.redirect("/?error=" + error)
+            params['error_password'] = "Invalid password!"
+            error_ind = True
 
         if password != ver_password:
-            error = "Passwords did not match"
-            self.redirect("/?error=" + error)
+            params['error_match'] = "Passwords did not match"
+            error_ind = True
 
         if not valid_email(email):
-            error = "Invalid email!"
-            self.redirect("/?error=" + error)
+            params['error_email'] = "Invalid email!"
+            error_ind = True
 
-    	self.response.write("Welcome " + username + "!")
+        if error_ind == False:
+            self.redirect('/welcome?username=' + username)
 
-
+class Welcome(webapp2.RequestHandler):
+    def get(self):
+        username = self.request.get("username")
+        self.response.write("Welcome " + username + "!")
 
 
 app = webapp2.WSGIApplication([
     ('/', Index),
-	('/verify', Verification)
+    ('/welcome', Welcome)
 ], debug=True)
